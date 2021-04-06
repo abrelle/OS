98833fdf1c9912c949f6dc852a92b5028ac9b353
Zivile <zivile.stunzenaite@gmail.com>
2021-04-06 21:29:30 +0300

    virtuali masina

--- src/VM/CPU.java
+++ src/VM/CPU.java
@@ -5,6 +5,10 @@ public class CPU {
     private final Word PTR = new Word(0); // Puslapių lentelės bazinis registras
     private final Word SP = new Word(0); // Steko rodyklė
 
+    private final Word[] RX = new Word[2];
+    private final Word RH = new Word(0);
+    private final Word RL = new Word(0);
+
     private final ByteWord MODE = new ByteWord(0); // Procesoriaus darbo režimas
     private final ByteWord C = new ByteWord(0);
 
@@ -21,6 +25,8 @@ public class CPU {
 
     CPU(Memory memory) throws Exception {
         this.memory = memory;
+        RX[0] = RH;
+        RX[1] = RL;
     }
 
     public Word getSP() throws Exception {
@@ -39,6 +45,12 @@ public class CPU {
     public Word getIC() { return IC; }
     public void setIC(Word word) { IC.setWord(word); }
     public void increaseIC() throws Exception { IC.setWord(IC.add(1)); }
+
+    public Word getRL() { return RL; }
+    public void setRL(Word word) { RL.setWord(word); }
+
+    public Word getRH() { return RH; }
+    public void setRH(Word word) { RH.setWord(word); }
 
     public void setDS(Word virtualAddress, Word value) throws Exception {
         memory.setWord(value,DS.getNumber() + virtualAddress.getNumber());
--- src/VM/VirtualMachine.java
+++ src/VM/VirtualMachine.java
@@ -1,13 +1,67 @@
 package VM;
 
+import java.util.ArrayList;
+
 public class VirtualMachine {
     private CPU cpu = null;
     private Memory memory = null;
-    private Commands interpretator = null;
+    private Commands interpreter = null;
 
-    VirtualMachine() throws Exception {
-        memory = new Memory();
-        cpu = new CPU(memory);
-        interpretator = new Commands(cpu,memory);
+    VirtualMachine()
+    {
+        try {
+            memory = new Memory();
+            cpu = new CPU(memory);
+            interpreter = new Commands(cpu,memory);
+            uploadCode();
+            doYourMagic();
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void doYourMagic()
+    {
+        int i = 0;
+        while (i<10)
+        {
+            try {
+                String command = memory.getWord(cpu.getCS(cpu.getIC())).getASCIIFormat();
+
+
+                interpreter.execute(command);
+                cpu.increaseIC();
+                i++;
+                if(command.contains("HALT"))
+                {
+                    return;
+                }
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void uploadCode()
+    {
+        try {
+            Interpreter interpreter =  new Interpreter("prog.txt");
+            interpreter.read();
+            interpreter.interpreter();
+            ArrayList<String> dataSegment = interpreter.getDataSegment();
+            for (int i = 0; i<dataSegment.size();i++)
+            {
+                cpu.setDS(new Word(i), new Word(dataSegment.get(i),Word.WORD_TYPE.NUMERIC));
+            }
+            ArrayList<String>codeSegment = interpreter.getCodeSegment();
+            for (int i = 0; i<codeSegment.size();i++)
+            {
+                cpu.setCS(new Word(i), new Word(codeSegment.get(i), Word.WORD_TYPE.SYMBOLIC));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
